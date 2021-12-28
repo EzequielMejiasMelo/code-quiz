5e1eea4460e6c7aa1ab0382abc52765ea9918322
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2021-12-27 22:50:36 -0500

    Update script.js
    Added questions variable and updated quizStart.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,17 +24,18 @@ var submitBtn = document.querySelector("#submit");
 // Starts and ends game
 function quizStart() {
   var score = quizGame();
+  
+  //sets up results elements
+  choicesDiv.setAttribute("class", "hide");
+  resultsDiv.setAttribute("class", "");
+  scoreEl.textContent = score;
 
   return;
 }
 
 //handles setting: questions, answers and timer | returns score int
 function quizGame() {
-  var questionsAndAnswers = {
-    question1: "Pizza?",
-    correctAnswer1: "No",
-    Answers1: ["Yes", "No", "Maybe"],
-  };
+  
 }
 
 function setHighscore() {
@@ -44,3 +45,42 @@ function setHighscore() {
 
 startBtn.addEventListener("click", quizStart);
 submitBtn.addEventListener("click", setHighscore);
+
+var questionsAndAnswers = [
+  {
+    question: "Which Greek hero was called 'Tamer of Horses'?",
+    answers: [
+      {text: "Paris", correct: false},
+      {text: "Hector", correct: true},
+      {text: "Alexander the Great", correct: false},
+      {text: "Apollo", correct: false}
+    ]
+  },
+  {
+    question: "In Greek mythology, who flew too close to the Sun?",
+    answers: [
+      {text: "Pegasus", correct: false},
+      {text: "Icarus", correct: true},
+      {text: "Theseus", correct: false},
+      {text: "Proteus", correct: false},
+    ]
+  },
+  {
+    question: "Who was the chief god of all the ancient Greeks?",
+    answers: [
+      {text: "Minerva", correct: false},
+      {text: "Zeus", correct: true},
+      {text: "Apollo", correct: false},
+      {text: "Hermes", correct: false},
+    ]
+  },
+  {
+    question: "Who was the Greek goddes of the rainbow?",
+    answers: [
+      {text: "Hera", correct: false},
+      {text: "Iris", correct: true},
+      {text: "Hecate", correct: false},
+      {text: "Aphrodite", correct: false},
+    ]
+  }
+]
