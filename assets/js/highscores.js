3bb3ef41de4a9a4493c95775d7dca59e0ec5911e
Ezequiel Mejias <ezequiel.mejiasmelo@gmail.com>
2022-01-02 17:34:52 -0500

    Wrote func to put scores on page

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,7 +1,22 @@
 // Assignment
 var clearBtn = document.querySelector("#clearScore");
+var backBtn = document.querySelector("#backBtn");
+var listEl = document.querySelector("#scoreList");
 
 function pullScores() {
+  var archive = [],
+    keys = Object.keys(localStorage),
+    i = 0, key;
+  
+  for (; key = keys[i]; i++){
+    archive.push(key + "=" + localStorage.getItem(key));
+
+    var listItem = document.createElement("li");
+    listItem.textContent = key + " - " + localStorage.getItem(key);
+    listEl.appendChild(listItem);
+  }
+
+  console.log(archive);
   return;
 }
 
@@ -11,5 +26,5 @@ function clearScores() {
 }
 
 //Event listeners
-window.onload = pullScores();
+window.addEventListener("load", pullScores);
 clearBtn.addEventListener("click", clearScores);
