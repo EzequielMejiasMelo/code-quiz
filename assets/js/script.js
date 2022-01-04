78c1e9d73389f8e61f594c9fa81954f6fe5c8409
Ezequiel Mejias <ezequiel.mejiasmelo@gmail.com>
2022-01-03 21:16:36 -0500

    Made change to element on index.html and completed question rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,18 @@ var questionEl = document.querySelector("#question");
 
 // choices
 var choicesDiv = document.querySelector("#choices");
-var choice1El = document.querySelector("#choice1");
-var choice2El = document.querySelector("#choice2");
-var choice3El = document.querySelector("#choice3");
-var choice4El = document.querySelector("#choice4");
+var choicesList = document.querySelector("#choicesList");
 
 //results section
 var resultsDiv = document.querySelector("#results");
 var scoreEl = document.querySelector("#score");
 var nameInput = document.querySelector("#initials");
 var submitBtn = document.querySelector("#submit");
+
+//state management variables
+var questionIndex = 0;
+var time = questionsAndAnswers.length * 15;
+var timerId; 
 
 // Starts and ends game
 function quizStart() {
@@ -37,8 +39,33 @@ function quizStart() {
 //handles setting: questions, answers and timer | returns score int
 function quizGame() {
   choicesDiv.setAttribute("class", "");
+  setCurrentQuestion();
   return;
 }
+
+function setCurrentQuestion() {
+  var currentQuestion = questionsAndAnswers[questionIndex];
+  questionEl.textContent = currentQuestion.question;
+
+  choicesList.textContent = "";
+
+  for (var i=0; i < currentQuestion.options.length; i++){
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "button");
+    choiceNode.setAttribute("value", currentQuestion.options[i]);
+
+    choiceNode.textContent = i + 1 + ". " + currentQuestion.options[i];
+
+    choicesList.appendChild(choiceNode);
+
+  }
+}
+
+choicesList.addEventListener("click", function(event){
+  if (event.target === "button") {
+    
+  }
+})
 
 function setHighscore() {
   localStorage.setItem(nameInput.value, scoreEl.textContent);
@@ -52,38 +79,22 @@ submitBtn.addEventListener("click", setHighscore);
 var questionsAndAnswers = [
   {
     question: "Which Greek hero was called 'Tamer of Horses'?",
-    answers: [
-      { text: "Paris", correct: false },
-      { text: "Hector", correct: true },
-      { text: "Alexander the Great", correct: false },
-      { text: "Apollo", correct: false },
-    ],
+    options: ["Paris", "Hector", "Alexander the Great", "Apollo"],
+    answer: "Hector"
   },
   {
     question: "In Greek mythology, who flew too close to the Sun?",
-    answers: [
-      { text: "Pegasus", correct: false },
-      { text: "Icarus", correct: true },
-      { text: "Theseus", correct: false },
-      { text: "Proteus", correct: false },
-    ],
+    options: ["Pegasus", "Icarus", "Theseus", "Proteus"],
+    answer: "Icarus"
   },
   {
     question: "Who was the chief god of all the ancient Greeks?",
-    answers: [
-      { text: "Minerva", correct: false },
-      { text: "Zeus", correct: true },
-      { text: "Apollo", correct: false },
-      { text: "Hermes", correct: false },
-    ],
+    options: ["Minerva", "Zeus", "Apollo", "Hermes"],
+    answer: "Zeus"
   },
   {
     question: "Who was the Greek goddes of the rainbow?",
-    answers: [
-      { text: "Hera", correct: false },
-      { text: "Iris", correct: true },
-      { text: "Hecate", correct: false },
-      { text: "Aphrodite", correct: false },
-    ],
+    options: ["Hera", "Iris", "Hecate", "Aphrodite"],
+    answer: "Iris"
   },
 ];
